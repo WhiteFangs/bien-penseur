3732a6ebb6275b8ce92a7ece36a926141c609015
WhiteFangs <white-fangs@hotmail.fr>
2017-01-02 18:32:42 +0100

    Added and improved word replacements, optimized dom replacement and handled dom tree change

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Bien penseur
 =============
 
-Une extension Chrome qui remplace des expressions de la mal-pensance par de jolies expressions de la bien-pensance (plutôt premier degré)
+Une extension Chrome qui remplace des expressions de la mal-pensance par de jolies expressions de la bien-pensance (plus ou moins premier degré)
 
 > Certaines personnes pensent étrangement que donner son opinion c'est de la liberté d'expression, mais que critiquer celle-ci est une sorte de censure.
 
@@ -15,35 +15,35 @@ Tableau des expressions
 | Expression    | Remplacement  | 
 | ------------- |:-------------:|
 | bobo(s) | citoyen(s) aisé(s), diplômé(s) et progressiste(s) |
-| bobosphère | _à définir_ |
+| bobosphère | réunion d’éclairés auto-déclarés |
 | bien(-)pensance | pensée bienveillante |
 | bien(-)pensant(e/s) | bienveillant(e/s) |
 | dissidence | divergence d'opinion |
 | dissident(e/s) | divergeant(e/s) d'opinion |
-| gauchisme | pensée de gauche |
+| gauchisme | mouvement de pensée de gauche |
 | gauchiste(s) | partisan(s) de gauche |
 | gaucho(s) | partisan(s) de gauche |
 | gauchiasse(s) | partisanne(s) de gauche |
-| islamo(-)gauchisme | _à définir_ |
-| islamo(-)gauchiste(s) | _à définir_ |
-| islamo(-)bolchévique(s) | _à définir_ |
-| judéo(-)bolchévique(s) | _à définir_ |
-| judéo(-)gauchiste(s) | _à définir_ |
-| judéo(-)chrétien(ne/s) | _à définir_ |
-| racine(s) chrétien(ne/s) | _à définir_ |
-| gaulois(e/s) | _à définir_ |
-| racisme anti(-)blanc(s) | _à définir_ |
-| journalope(s) | _à définir_ |
-| merdia(s) | _à définir_ |
-| bisounours | _à définir_ |
-| assisté(e/s) | _à définir_ |
-| fémi(-)nazi(e/s) | _à définir_ |
-| grand remplacement | _à définir_ |
-| multi(-)culturalisme | _à définir_ |
-| multi(-)culturaliste(s) | _à définir_ |
-| nouvel ordre mondial | _à définir_ |
-| nouvel ordre moral | _à définir_ |
-| pensée unique | _à définir_ |
-| procès stalinien | _à définir_ |
-| politburo | _à définir_ |
-| police(s) de la pensée | _à définir_ |
+| islamo(-)gauchisme | esprit de gauche pour la diversité culturelle du couscous |
+| islamo(-)gauchiste(s) | individu(s) de gauche pour la diversité culturelle du couscous |
+| islamo(-)bolchévique(s) | esprit(s) de gauche pour la diversité culturelle du keftah |
+| judéo(-)bolchévique(s) | esprit(s) de gauche pour la diversité culturelle du falafel |
+| judéo(-)gauchiste(s) | individu(s) de gauche pour la diversité culturelle du falafel |
+| judéo(-)chrétien(ne/s) | d’héritage de l’Ancien et du Nouveau Testament, des Chroniques, du Livre des Rois, du Pentateuque, du Deutéronome, de la Genèse, et du Livre des Juges |
+| racine(s) chrétienne(s) | souche(s) d’arbre(s) héritée(s) de Jésus |
+| gaulois(e/s) | Vercingétorix |
+| racisme anti(-)blanc(s) | soutien aux discriminés raciaux |
+| journalope(s) | rédacteur(s) de journal(/ux) |
+| merdia(s) | support(s) d’informations |
+| bisounours | citoyen avec des sentiments exacerbés |
+| assisté(e/s) | individu ne payant pas l’ISF |
+| fémi(-)nazi(e/s) | militant(e/s) virulent(e/s) pour les droits des femmes |
+| grand remplacement | développement démographique à forte diversité culturelle |
+| multi(-)culturalisme | mouvement pour la diversité des cultures |
+| multi(-)culturaliste(s) | citoyen pour la diversité des cultures |
+| nouvel ordre mondial | changement de la géopolitique mondiale au nouveau millénaire |
+| nouvel ordre moral | changement des catégorisations de pensée |
+| pensée unique | pensée décrite comme partagée par la plupart des gens mais en fait non |
+| procès stalinien(s) | débat(s) contradictoire(s) |
+| politburo | secrétariat général de la haute autorité |
+| police(s) de la pensée | contradiction(s) argumentée(s) et éclairé(s) |
--- Source/content_script.js
+++ Source/content_script.js
@@ -5,6 +5,7 @@
 var replacements = {
 	"bobo" : "citoyen aisé, diplômé et progressiste", 
 	"bobos" : "citoyens aisés, diplômés et progressistes", // define plurals
+	"bobosphère" : "réunion d'éclairés auto-déclarés",
 	"bien-pensance" : "pensée bienveillante", // replaces also "bienpensance"
 	"bien-pensant" : "bienveillant",
 	"bien-pensants" : "bienveillants",
@@ -15,13 +16,58 @@ var replacements = {
 	"dissidents" : "divergeants d'opinion",
 	"dissidente" : "divergeante d'opinion",
 	"dissidentes" : "divergeantes d'opinion",
-	"gauchisme" : "pensée de gauche",
+	"gauchisme" : "mouvement de pensée de gauche",
 	"gauchiste" : "partisan de gauche",
 	"gauchistes" : "partisans de gauche",
 	"gaucho" : "partisan de gauche",
 	"gauchos" : "partisans de gauche",
 	"gauchiasse" : "partisanne de gauche",
 	"gauchiasses" : "partisannes de gauche",
+	"islamo-gauchisme" : "esprit de gauche pour la diversité culturelle du couscous",
+	"islamo-gauchiste" : "individu de gauche pour la diversité culturelle du couscous",
+	"islamo-gauchistes" : "individus de gauche pour la diversité culturelle du couscous",
+	"islamo-bolchévique" : "esprit de gauche pour la diversité culturelle du keftah",
+	"islamo-bolchéviques" : "esprits de gauche pour la diversité culturelle du keftah",
+	"judéo-bolchévique" : "esprit de gauche pour la diversité culturelle du falafel",
+	"judéo-bolchéviques" : "esprits de gauche pour la diversité culturelle du falafel",
+	"judéo-gauchiste" : "individu de gauche pour la diversité culturelle du falafel",
+	"judéo-gauchistes" : "individus de gauche pour la diversité culturelle du falafel",
+	"judéo-chrétien" : "d’héritage de l’Ancien et du Nouveau Testament, des Chroniques, du Livre des Rois, du Pentateuque, du Deutéronome, de la Genèse, et du Livre des Juges",
+	"judéo-chrétiens" : "d’héritage de l’Ancien et du Nouveau Testament, des Chroniques, du Livre des Rois, du Pentateuque, du Deutéronome, de la Genèse, et du Livre des Juges",
+	"judéo-chrétienne" : "d’héritage de l’Ancien et du Nouveau Testament, des Chroniques, du Livre des Rois, du Pentateuque, du Deutéronome, de la Genèse, et du Livre des Juges",
+	"judéo-chrétiennes" : "d’héritage de l’Ancien et du Nouveau Testament, des Chroniques, du Livre des Rois, du Pentateuque, du Deutéronome, de la Genèse, et du Livre des Juges",
+	"racine chrétienne" : "souche d’arbre héritée de Jésus",
+	"racines chrétiennes" : "souches d’arbres héritées de Jésus",
+	"gaulois" : "Vercingétorix",
+	"gauloise" : "Vercingétorix",
+	"gauloises" : "Vercingétorix",
+	"racisme anti-blanc" : "soutien aux discriminés raciaux",
+	"racisme anti-blancs" : "soutien aux discriminés raciaux",
+	"journalope" : "rédacteur de journal",
+	"journalopes" : "rédacteurs de journaux",
+	"merdia" : "support d’informations",
+	"merdias" : "supports d’informations",
+	"bisounours" : "citoyens avec des sentiments exacerbés",
+	"assisté" : "individu ne payant pas l’ISF",
+	"assistée" : "personne ne payant pas l’ISF",
+	"assistés" : "individus ne payant pas l’ISF",
+	"assistées" : "personnes ne payant pas l’ISF",
+	"fémi-nazi" : "militant virulent pour les droits des femmes",
+	"fémi-nazie" : "militante virulente pour les droits des femmes",
+	"fémi-nazis" : "militants virulents pour les droits des femmes",
+	"fémi-nazies" : "militantes virulentes pour les droits des femmes",
+	"grand remplacement" : "développement démographique à forte diversité culturelle",
+	"multi-culturalisme" : "mouvement pour la diversité des cultures",
+	"multi-culturaliste" : "citoyen pour la diversité des cultures",
+	"multi-culturalistes" : "citoyens pour la diversité des cultures",
+	"nouvel ordre mondial" : "changement de la géopolitique mondiale au nouveau millénaire",
+	"nouvel ordre moral" : "changement des catégorisations de pensée",
+	"pensée unique" : "pensée décrite comme partagée par la plupart des gens mais en fait non",
+	"procès stalinien" : "débat contradictoire",
+	"procès staliniens" : "débats contradictoires",
+	"politburo" : "secrétariat général de la haute autorité",
+	"police de la pensée" : "contradiction argumentée et éclairée",
+	"polices de la pensée" : "contradictions argumentées et éclairées",
 };
 
 walk(document.body);
@@ -55,15 +101,19 @@ function walk(node)
 function handleText(textNode) 
 {
 	var v = textNode.nodeValue;
-
-	Object.keys(replacements).map(function(expression, index) {
-		var remplacement = replacements[expression];
-		v = replace(v, expression, remplacement);
-		if(expression.indexOf("-"))
-			v = replace(v, expression.replace("-", ""), remplacement);
-	});
+	var lowerV = v.toLowerCase();
+	if(lowerV.trim().length > 0){
+		Object.keys(replacements).map(function(expression, index) {
+			if(lowerV.indexOf(expression) > -1 || (expression.indexOf("-") > -1 && lowerV.indexOf(expression.replace("-", "")))){
+				var remplacement = replacements[expression];
+				if(lowerV.indexOf(expression) > -1)
+					v = replace(v, expression, remplacement);
+				if(expression.indexOf("-") > -1 && lowerV.indexOf(expression.replace("-", "")))
+					v = replace(v, expression.replace("-", ""), remplacement);
+			}
+		});
+	}
 	textNode.nodeValue = v;
-
 }
 
 
@@ -81,3 +131,18 @@ function replace(str, before, after) {
 function getBeforeRegex(before){
 	return new RegExp("\\b" + before.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&') + "\\b", 'g');
 }
+
+var timeout = null;
+var walking = false;
+document.addEventListener("DOMSubtreeModified", function() {
+	if(!walking){
+		if(timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(function(){ 
+			walking = true;
+			walk(document.body);
+			walking = false;
+		}, 500);
+	}
+}, false);
